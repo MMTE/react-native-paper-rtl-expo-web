4696c7a3a00f7df259b0673a26f40e011724f012
MMTE <hi.taleghani@gmail.com>
2023-03-08 20:03:55 +0330

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Appbar} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {I18nManager} from "react-native";
+import {I18nManager as I18nManagerWeb } from "react-native-web";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    I18nManager.forceRTL(true);
+    I18nManagerWeb.forceRTL(true);
+
+    return (
+        <PaperProvider>
+            <View>
+                <Appbar.Header>
+                    <Appbar.BackAction onPress={() => {
+                    }}/>
+                    <Appbar.Content title="سلام!"/>
+                    <Appbar.Action icon="calendar" onPress={() => {
+                    }}/>
+                    <Appbar.Action icon="magnify" onPress={() => {
+                    }}/>
+                </Appbar.Header>
+            </View>
+        </PaperProvider>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
